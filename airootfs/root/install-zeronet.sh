7c877615eb685474a3742e17dbe519acc1ef307b
Obscurix <Obscurix@protonmail.ch>
2019-09-07 23:15:33 +0000

    Update Zeronet to 0.7.1

--- airootfs/root/install-zeronet.sh
+++ airootfs/root/install-zeronet.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
 # Install Zeronet.
-zeronet_ver="0.6.5"
+zeronet_ver="0.7.1"
 zeronet_dir="/usr/src/ZeroNet-${zeronet_ver}"
 
 # Download and extract source.
 wget --https-only https://github.com/HelloZeroNet/ZeroNet/archive/v${zeronet_ver}.tar.gz -O /usr/src/zeronet.tar.gz
 tar -xf /usr/src/zeronet.tar.gz -C /usr/src
-
-## THIS PART IS NOT READY YET. WILL BE UNCOMMENTED IN THE FUTURE
-#
-# Download source.
-#git clone https://github.com/HelloZeroNet/ZeroNet ${zeronet_dir}
-#
-# Import the ZeroNet signing key.
-#
-# This key is the exact same as found on Github.
-#
-# https://github.com/HelloZeroNet/ZeroNet/issues/759
-#
-#gpg --recv-keys "960F FF2D 6C14 5AA6 13E8 491B 5B63 BAE6 CB96 13AE"
-#gpg --fingerprint "960F FF2D 6C14 5AA6 13E8 491B 5B63 BAE6 CB96 13AE" &>/dev/null
-#
-# Verify ZeroNet.
-#cd ${zeronet_dir}
-#
-#if ! git log --show-signature | head -n 8 | grep "Good signature"; then
-#  echo "ERROR: ZERONET CANNOT BE VERIFIED."
-#  exit 1
-#fi
-#
-##
 
 # Create needed directory.
 mkdir -pm 755 /opt/zeronet
