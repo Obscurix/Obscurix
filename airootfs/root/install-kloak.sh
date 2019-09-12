574379af919b2979ea08442cb8a7350f8463ebab
Obscurix <Obscurix@protonmail.ch>
2019-09-12 21:55:09 +0000

    Make kloak executable

--- airootfs/root/install-kloak.sh
+++ airootfs/root/install-kloak.sh
@@ -8,7 +8,7 @@ git clone https://github.com/vmonaco/kloak ${kloak_dir}
 # Compile and install kloak.
 cd ${kloak_dir}
 make all
-install -m644 ${kloak_dir}/kloak /usr/bin/kloak
+install -m744 ${kloak_dir}/kloak /usr/bin/kloak
 install -m744 ${kloak_dir}/lib/systemd/system/kloak.service /lib/systemd/system/kloak.service
 
 # We don't use /usr/sbin.
