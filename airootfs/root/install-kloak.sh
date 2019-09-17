e097b9ba03ac28af0428ee78cabc2fec4e0e0609
Obscurix <Obscurix@protonmail.ch>
2019-09-17 17:36:24 +0000

    Use kloak AppArmor profile

--- airootfs/root/install-kloak.sh
+++ airootfs/root/install-kloak.sh
@@ -10,9 +10,12 @@ cd ${kloak_dir}
 make all
 install -m744 ${kloak_dir}/kloak /usr/bin/kloak
 install -m744 ${kloak_dir}/lib/systemd/system/kloak.service /lib/systemd/system/kloak.service
+install -m644 ${kloak_dir}/etc/apparmor.d/usr.sbin.kloak /etc/apparmor.d/usr.bin.kloak
 
-# We don't use /usr/sbin.
+# We don't use /usr/sbin or local AppArmor files.
 sed -i 's/\/usr\/sbin\/kloak/\/usr\/bin\/kloak/g' /lib/systemd/system/kloak.service
+sed -i s/\/usr\/sbin\/kloak/\/usr\/bin\/kloak/g' /etc/apparmor.d/usr.bin.kloak
+sed -i 's/#include <local\/usr.sbin.kloak>//' /etc/apparmor.d/usr.bin.kloak
 
 # Enable the systemd service.
 systemctl enable kloak.service
